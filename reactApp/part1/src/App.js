d9d39adc2f1745df75f86aac35435d9363eced0c
Ricardo Tlatelpa <ricardotlatelpa@Ricardos-MacBook-Pro.local>
2023-06-06 18:44:10 -0400

    props

--- reactApp/part1/src/App.js
+++ reactApp/part1/src/App.js
@@ -1,9 +1,30 @@
 import './App.css';
 
+const Hello = (props) => {
+    
+  const bornYear = () => {
+    return new Date().getFullYear() - props.age;
+  }
+
+  return (
+   <div> 
+    <p>
+      Hello {props.name}, you are {props.age} years old.
+    </p>
+    <p>
+    You were probably born in {bornYear()}!
+    </p>
+  </div>
+  )
+}
 function App() {
+  const name = "Peter"
+  const age = 10
   return (
     <div>
       <p> Hello World! </p>
+      <Hello name = "Ricardo" age = {24} />
+      <Hello name = {name} age = {age} /> 
     </div>
   )
 }
